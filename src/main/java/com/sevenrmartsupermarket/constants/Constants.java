0d4558b19f6876de249f6fd0b06cfead6f5e7550
anptv-08 <anptv08@gmail.com>
2024-01-15 18:28:15 +0530

    Remove deleted folders

--- src/main/java/com/sevenrmartsupermarket/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.sevenrmartsupermarket.constants;
-
-public class Constants {
-public static final String CONFIG_FILE_PATH=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties"; // file path of Config.properies files 
-//System.getProperty("user.dir") will return project path in local+ remaining path of folder is same for everyone.
-public static final String EXCEL_FILE_PATH=System.getProperty("user.dir")+"\\src\\main\\resources\\Excel_Files\\";
-public static final String SCREENSHOT_FILE_PATH=System.getProperty("user.dir")+"\\ScreenShots\\";
-public static final String EXTENT_REPORT_PATH=System.getProperty("user.dir")+"\\Extent_Reports";
-}
